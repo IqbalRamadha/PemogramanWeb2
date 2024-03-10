e1da414d168c372ad5d9a41883fc3cb1ef11c8ec
M iqbal_0110223280 <148035714+IqbalRamadha@users.noreply.github.com>
2024-03-10 08:36:02 +0700

    Add files via upload

---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,11 @@
+<?php 
+$domisili = ["Jakarta","Bogor","Depok","Tanggerang","Bekasi"];
+
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital",
+];    
+    
+
+?>
